f297abda1ef368ab56ac6ce194aa966ec76828e3
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-06 22:37:54 +0200

    Optimize desktop view

--- src/App.js
+++ src/App.js
@@ -19,16 +19,34 @@ class App extends React.Component {
           <p>(The City That Unites)</p>
         </nav>
         <div className="app-container">
-          <section role="list of places in budapest">
-            <input/>
-            <ul className="placesList">
+          <section className='list-section' role="list of places in budapest">
+            <input placeholder='Search for Location'/>
+            <ul>
+              <li>numoro 1</li>
+              <li>numoro 3</li>
+              <li>hier ist 5.</li>
+              <li>das ist kiko</li>
+              <li>numoro 1</li>
+              <li>numoro 3</li>
+              <li>hier ist 5.</li>
+              <li>das ist kiko</li>
+              <li>numoro 1</li>
+              <li>numoro 3</li>
+              <li>hier ist 5.</li>
+              <li>das ist kiko</li>
+              <li>numoro 1</li>
+              <li>numoro 3</li>
+              
+              <li>das ist kiko</li>
               <li>numoro 1</li>
               <li>numoro 3</li>
               <li>hier ist 5.</li>
               <li>das ist kiko</li>
             </ul>
           </section>
+          <section className='map-section'>
           <div id="map" />
+          </section>
         </div>
       </div>
     );
