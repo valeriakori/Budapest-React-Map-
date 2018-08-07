442ec77473955dabab1fd6337bd77ed4304801ad
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-07 22:12:57 +0200

    Outsource marker to List component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./index.css";
 import List from "./List";
 
 class App extends React.Component {
+
+  state = {
+    map: ''
+  }
   componentDidMount() {
 
     let map = new window.google.maps.Map(document.getElementById("map"), {
@@ -10,33 +14,21 @@ class App extends React.Component {
       zoom: 13
     })
 
-    let casaBaja = { lat: 47.509154, lng: 19.055431 }
 
-    let marker = new window.google.maps.Marker({
-      position: casaBaja,
-      map: map,
-      animation: window.google.maps.Animation.DROP,
 
-      title: 'first marker'
-    })
+    // marker.addListener('click', () => infoWindow.open(map, marker))
 
-    let infoWindow = new window.google.maps.InfoWindow({
-      content: marker.position.toString()
-    })
-
-    marker.addListener('click', () => infoWindow.open(map, marker))
-
-    marker.addListener('click', () => {
-      if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-      } else {
-        marker.setAnimation(window.google.maps.Animation.BOUNCE);
-        setTimeout(() => { marker.setAnimation(null); }, 750);
-      }
-    });
+    // marker.addListener('click', () => {
+    //   if (marker.getAnimation() !== null) {
+    //     marker.setAnimation(null);
+    //   } else {
+    //     marker.setAnimation(window.google.maps.Animation.BOUNCE);
+    //     setTimeout(() => { marker.setAnimation(null); }, 750);
+    //   }
+    // });
 
     
-
+    this.setState({ map: map })
 
 
   }
@@ -50,7 +42,7 @@ class App extends React.Component {
           <p>(The City That Unites)</p>
         </nav>
         <div className="app-container">
-          <List />
+          <List map={this.state.map}/>
           <section className="map-section">
             <div id="map" />
           </section>
