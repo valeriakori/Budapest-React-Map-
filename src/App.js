3d6ace1bb3a1e18ff71bd62a9f1ee7647db653d1
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-08 18:57:50 +0200

    Outsource map to List

--- src/App.js
+++ src/App.js
@@ -3,36 +3,6 @@ import "./index.css";
 import List from "./List";
 
 class App extends React.Component {
-
-  state = {
-    map: ''
-  }
-  componentDidMount() {
-
-    let map = new window.google.maps.Map(document.getElementById("map"), {
-      center: { lat: 47.497912, lng: 19.040235 },
-      zoom: 13
-    })
-
-
-
-    // marker.addListener('click', () => infoWindow.open(map, marker))
-
-    // marker.addListener('click', () => {
-    //   if (marker.getAnimation() !== null) {
-    //     marker.setAnimation(null);
-    //   } else {
-    //     marker.setAnimation(window.google.maps.Animation.BOUNCE);
-    //     setTimeout(() => { marker.setAnimation(null); }, 750);
-    //   }
-    // });
-
-    
-    this.setState({ map: map })
-
-
-  }
-
   render() {
     return (
       <div>
@@ -41,12 +11,7 @@ class App extends React.Component {
           <br />
           <p>(The City That Unites)</p>
         </nav>
-        <div className="app-container">
-          <List map={this.state.map}/>
-          <section className="map-section">
-            <div id="map" />
-          </section>
-        </div>
+        <List />
       </div>
     );
   }
