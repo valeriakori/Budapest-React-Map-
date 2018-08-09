a5e713ad5cf0e71f8542c0f8c1c4b2b421b212b6
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-09 13:31:57 +0200

    Add credentials (refactor later)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import "./index.css";
 import List from "./List";
 
 class App extends React.Component {
+
+  state = {
+    CLIENT_ID: "VPTQVDF2IZ4WXJE01KPUJEOPQLY3F1GSLYBTAV2IEWD5UBUY",
+    CLIENT_SECRET: "M1KN4D4LCNH5Q1XGEPKLO3P125YQH3JNYTL4VQJJZIR0SCE1",
+    version: 20180809
+  };
   render() {
     //const { CLIENT_ID, CLIENT_SECRET, version } = this.state
     return (
